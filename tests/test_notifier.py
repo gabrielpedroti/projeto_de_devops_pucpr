11421f69852e6cd009b461e6d9d1d5ac02c91283
Gabriel Pedroti <gabrielpedroti@icloud.com>
2025-09-14 16:05:18 -0300

    test: adiciona raiz ao sys.path para importar src

--- tests/test_notifier.py
+++ tests/test_notifier.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# garante que a raiz do repo esteja no PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 from src.notifier import send_notification
 
 
